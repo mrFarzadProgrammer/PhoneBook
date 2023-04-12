76abbab9946762f60f8d3e0650292fa8fff74beb
mrProgrammer <farzadmohamadi201@gmail.com>
2023-04-12 14:20:35 +0330

    add service in BLL layer to GetContactLists

---
+++ BLL/Services/ContactService.cs
@@ -0,0 +1,27 @@
+﻿using BLL.Dto;
+using DAL.DataBase;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL.Services
+{
+    public class ContactService
+    {
+        DataBaseContext dataBase = new DataBaseContext();
+
+        /// <summary>
+        /// دریافت لیست مخاطبان
+        /// </summary>
+        /// <returns></returns>
+        public List<ContactListDto> GetContactLists()
+        {
+            var contacts = dataBase.Contacts.Select(c => new ContactListDto
+            {
+                Id = c.Id,
+                FullName = $"{c.Name} {c.LastName}",
+                PhoneNumber = c.PhoneNumber
+            }).ToList();
+            return contacts;
+        }
+    }
+}
